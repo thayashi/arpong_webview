cc772927f2e6be94601f36b3374c5b0466bece35
Toshiyuki Hayashi <thayashing@gmail.com>
2016-10-29 11:08:40 -0700

    Change Scale

--- src/js/app.js
+++ src/js/app.js
@@ -34,10 +34,11 @@ var SequenceView = Backbone.View.extend({
   },
 
   activate: function(number) {
-    for (let i = 0; i < this.sequence.length; i++) {
-      this.sequence[i].$el.removeClass('on');
+    var self = this;
+    for (let i = 0; i < self.sequence.length; i++) {
+      self.sequence[i].$el.removeClass('on');
     }
-    this.sequence[number].$el.addClass('on');
+    self.sequence[number].$el.addClass('on');
   },
 
   render: function() {
@@ -59,7 +60,8 @@ var GridView = Backbone.View.extend({
   className: 'grid',
 
   events: {
-    'mouseenter': 'zoom'
+    'mouseenter': 'zoom',
+    'touchstart': 'zoom'
   },
 
   initialize: function(option) {
@@ -75,11 +77,15 @@ var GridView = Backbone.View.extend({
     this.vxplus = 0.2;
     this.rotate = 0;
     this.is_bang = false;
+    this.is_collision = false;
   },
 
   update: function() {
     this.vx = (this.vx + (this.gzoom_base - this.gzoom)/this.accel)/this.slow;
     this.gzoom = this.gzoom + this.vx;
+    if (this.is_collision) {
+      this.rotate += 12;
+    }
     //this.rotate += 10;
     this.el.style.transform = "rotate(" + this.rotate + "deg) scale(" + this.gzoom +")";
   },
@@ -96,9 +102,19 @@ var GridView = Backbone.View.extend({
     this.is_bang = true;
   },
 
+  bang_collision: function(value) {
+    this.vx += value * 1.5;
+    this.$el.addClass('on collision');
+    this.is_bang = true;
+    this.is_collision = true;
+  },
+
   reset: function() {
     this.$el.removeClass('on');
+    this.$el.removeClass('collision');
     this.is_bang = false;
+    this.is_collision = false;
+    this.rotate = 0;
   },
 
   render: function() {
@@ -108,8 +124,6 @@ var GridView = Backbone.View.extend({
     this.el.style.top =  this.y + 'px';
     if (window.fullcolor) {
       this.el.style.backgroundColor = '#'+(0x1000000+(Math.random())*0xffffff).toString(16).substr(1,6);
-    } else {
-      this.el.style.backgroundColor =  '#f00';
     }
     return this;
   }
@@ -134,6 +148,7 @@ var AppView = Backbone.View.extend({
     this.start_time = new Date().getTime();
     this.sequence_count = 0;
     this.interval = 1000;
+    window.matrix = this.matrix;
   },
 
   update: function() {
@@ -146,9 +161,10 @@ var AppView = Backbone.View.extend({
   },
 
   resets: function() {
-    for (let i = 0; i < this.matrix_y; i++) {
-      for (let j = 0; j < this.matrix_x; j++) {
-        this.matrix[i][j].reset();
+    var self = this;
+    for (let i = 0; i < self.matrix_y; i++) {
+      for (let j = 0; j < self.matrix_x; j++) {
+        self.matrix[i][j].reset();
       }
     }
   },
@@ -163,6 +179,16 @@ var AppView = Backbone.View.extend({
       let grid = this.matrix[array[i]][count];
       grid.is_bang = true;
     }
+
+    let collision = (Math.random() < 0.2) ? true : false;
+
+    if (collision) {
+      let collision_grid = this.matrix[array[select_number]][count];
+      let bang_value = 0.1 + 0.3 * Math.random();
+      collision_grid.bang_collision(bang_value);
+    }
+
+
 
     for (let i = 0; i < this.matrix_y; i++) {
       let grid = this.matrix[i][count];
@@ -197,7 +223,6 @@ var AppView = Backbone.View.extend({
       }
       this.sequenceView.activate(this.sequence_count);
       this.bangs(this.sequence_count);
-      console.log(this.sequence_count);
      }
 
     window.requestAnimationFrame(() => {this.sequencer()});
@@ -205,18 +230,21 @@ var AppView = Backbone.View.extend({
 
 
   render: function() {
+    var self = this;
     this.sequenceView = new SequenceView({
       matrix_x: this.matrix_x,
       grid_size: this.grid_size
     });
     this.$el.append(this.sequenceView .render().el);
     this.sequenceView.activate(0);
-
+    window.sequence_active = function(num) {
+      window.app.sequenceView.activate(num);
+    };
     for (let i = 0; i < this.matrix_y; i++) {
       for (let j = 0; j < this.matrix_x; j++) {
         this.matrix[i][j] = new GridView({
           x: 25 * (j + 1) + this.grid_size * j + 29,
-          y: 20 * (i + 3) + this.grid_size * i + 20,
+          y: 14 * (i + 3) + this.grid_size * i,
           size: this.grid_size
         });
         this.$el.append(this.matrix[i][j].render().el);
@@ -224,7 +252,9 @@ var AppView = Backbone.View.extend({
     }
     window.requestAnimationFrame(() => {
       this.update();
-      this.sequencer();
+      if (window.sequencer) {
+        this.sequencer();
+      }
     });
   }
 
@@ -233,10 +263,23 @@ var AppView = Backbone.View.extend({
 var app = new AppView;
 app.render();
 
+window.resetsAll = function() {
+  window.app.resets();
+};
+
 window.changeBackgroundImage = function(num) {
   $('.app-body').css({
     'background-image': 'url(../img/' +  num + '.png)'
-  })
-
+  });
 };
+
+window.changeBackgroundColor = function() {
+  $('body').css({
+    'background-color': '#'+(0x1000000+(Math.random())*0xffffff).toString(16).substr(1,6)
+  });
+};
+
+document.body.addEventListener('touchmove', function(event) {
+  event.preventDefault();
+}, false);
 
